5c9a9db0ce8d19f6165c95ef4c187484b7fbb46b
scimania <gauravdeepubhi@gmail.com>
2023-12-26 00:40:13 +0530

    added required packages and some base settings for the website

---
+++ src/app/fonts.ts
@@ -0,0 +1,11 @@
+import { Open_Sans, Playfair_Display } from "next/font/google";
+
+export const openSans = Open_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const playfairDisplay = Playfair_Display({
+  subsets: ["latin"],
+  display: "swap",
+});
